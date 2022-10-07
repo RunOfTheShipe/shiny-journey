a8dac89aedd370aba0120282b36b56517b7f7755
Brian Shipe <bmshipe@gmail.com>
2022-10-07 13:56:29 -0500

    server to support receiving a request

--- pong-server/main.go
+++ pong-server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,10 +10,20 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/pong", getPong)
+	router.POST("/aircraft", processAircraft)
 
 	router.Run("localhost:6543")
 }
 
 func getPong(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "Pong")
+}
+
+func processAircraft(c *gin.Context) {
+	var request AircraftRequest
+	if err := c.BindJSON(&request); err != nil {
+		return
+	}
+
+	fmt.Printf("Process request: timestamp=%f messages=%d\n", request.Now, request.Messages)
 }
---
+++ pong-server/types.go
@@ -0,0 +1,45 @@
+package main
+
+type AircraftRequest struct {
+	Now      float64    `json:"now"`
+	Messages int        `json:"messages"`
+	Aircraft []Aircraft `json:"aircraft"`
+}
+
+type Aircraft struct {
+	Hex            string        `json:"hex"`
+	AltBaro        int           `json:"alt_baro,omitempty"`
+	Version        int           `json:"version,omitempty"`
+	NacP           int           `json:"nac_p,omitempty"`
+	Sil            int           `json:"sil,omitempty"`
+	SilType        string        `json:"sil_type,omitempty"`
+	Mlat           []interface{} `json:"mlat"`
+	Tisb           []interface{} `json:"tisb"`
+	Messages       int           `json:"messages"`
+	Seen           float64       `json:"seen"`
+	Rssi           float64       `json:"rssi"`
+	Flight         string        `json:"flight,omitempty"`
+	AltGeom        int           `json:"alt_geom,omitempty"`
+	Gs             float64       `json:"gs,omitempty"`
+	Track          float64       `json:"track,omitempty"`
+	BaroRate       int           `json:"baro_rate,omitempty"`
+	Squawk         string        `json:"squawk,omitempty"`
+	Emergency      string        `json:"emergency,omitempty"`
+	Category       string        `json:"category,omitempty"`
+	NavQnh         float64       `json:"nav_qnh,omitempty"`
+	NavAltitudeMcp int           `json:"nav_altitude_mcp,omitempty"`
+	NavModes       []string      `json:"nav_modes,omitempty"`
+	Lat            float64       `json:"lat,omitempty"`
+	Lon            float64       `json:"lon,omitempty"`
+	Nic            int           `json:"nic,omitempty"`
+	Rc             int           `json:"rc,omitempty"`
+	SeenPos        float64       `json:"seen_pos,omitempty"`
+	NicBaro        int           `json:"nic_baro,omitempty"`
+	NacV           int           `json:"nac_v,omitempty"`
+	Gva            int           `json:"gva,omitempty"`
+	Sda            int           `json:"sda,omitempty"`
+	GeomRate       int           `json:"geom_rate,omitempty"`
+	NavHeading     float64       `json:"nav_heading,omitempty"`
+	Type           string        `json:"type,omitempty"`
+	TrueHeading    float64       `json:"true_heading,omitempty"`
+}
